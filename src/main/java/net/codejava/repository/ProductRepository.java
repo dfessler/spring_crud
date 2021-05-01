c4a5b26b227b6192880ce5d650c3b407f2f3b844
dominic <dfessler@tsn.at>
2021-05-01 18:27:50 +0200

    Done

---
+++ src/main/java/net/codejava/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package net.codejava.repository;
+
+import net.codejava.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long>
+{
+
+}
